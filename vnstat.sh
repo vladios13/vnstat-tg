617e96eb0ad30dbb675a7beebe6d509146511c8a
vladios13 <vladios13@yandex.ua>
2020-08-25 19:44:01 +0300

    Fix script description

--- vnstat.sh
+++ vnstat.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+# Вы можете запускать скрипт по расписанию с помощью crontab
+
+#        _           _ _           _ _____ 
+# __   _| | __ _  __| (_) ___  ___/ |___ / 
+# \ \ / / |/ _` |/ _` | |/ _ \/ __| | |_ \ 
+#  \ V /| | (_| | (_| | | (_) \__ \ |___) |
+#   \_/ |_|\__,_|\__,_|_|\___/|___/_|____/ 
+
 
 # Обновляем БД
 vnstat -u 
@@ -8,7 +16,7 @@ vnstati -s -i eth0 -o ~/tgvn.png
 # Генерируем картинку за день
 vnstati -d -i eth0 -o ~/tgvnd.png 
 
-echo "Готовлю пост в TG"
+echo "Готовлю пост в Telegram"
 sleep 1
 # chatID - id канала/группы/пользователя. botToken - токен бота. 
 chatID=1521654
@@ -28,4 +36,5 @@ shift
 # Отправляем вторую картинку
 curl -s "https://api.telegram.org/bot"$botToken"/sendPhoto" -F chat_id=$chatID -F "photo=@$photo_d" -F "caption=$MESSAGE_d"
 sleep 1
-exit 
+# Завершаем работу скрипта
+exit
